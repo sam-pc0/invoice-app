55effdf3131cf23d7fee4f6b4720b2751000825a
unknown <canxega@hotmail.com>
2021-07-06 13:06:25 -0600

    creation of server.js

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hola mundo");
+});
+
+app.listen(3000, () => {
+  console.log("servidor iniciado...");
+});
