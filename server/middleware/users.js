1dcfc8860f8bcfc8363725709d1c1cda5457e0a4
unknown <canxega@hotmail.com>
2021-07-21 22:43:49 -0600

    end point added

--- server/middleware/users.js
+++ server/middleware/users.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   validateRegister: (req, res, next) => {
     // username min length 3
-    if (!req.body.username || req.body.username.length < 1) {
+    if (!req.body.username || req.body.username.length < 3) {
       return res.status(400).send({
         msg: "Please enter a username with min. 3 chars",
       });
