d40e7a4ad17a4148757cd503b2361de1b81b3bb1
Koke Gudiel <kokegudiel2@gmail.com>
2021-08-21 09:55:03 -0600

    Database connection created

---
+++ db/db.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type MySqlClient struct {
+	*sqlx.DB
+}
+
+func NewSqlClient() *MySqlClient {
+	connStr := "root:Ops.@(64.227.28.17)/invoiceappdb"
+	db, err := sqlx.Open("mysql", connStr)
+	if err != nil {
+		log.Fatalln("[DB Connection Error]", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Println("[DB Connection Error]", err)
+	}
+
+	return &MySqlClient{db}
+}
